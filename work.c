dbad4c33e706ab89268b8dd1506ffd4012833e07
hanyu0000 <3634802280@qq.com>
2024-02-17 22:56:38 +0800

    3

--- work.c
+++ work.c
@@ -8,8 +8,6 @@
 #include <time.h>
 #include <pwd.h>
 #include <grp.h>
-
-#define MAX_PATH 1024
 /*
 DIR结构体：DIR结构体用于表示目录流。它是一个不透明的结构，通常由目录操作函数返回。在程序中，你不需要直接访问它的成员，只需将它作为参数传递给相关的目录操作函数即可。
   d_ino：    inode 号码，是文件系统中唯一标识文件或目录的值。
@@ -22,20 +20,15 @@ opendir函数：opendir函数用于打开一个目录，并返回一个指向DIR
 readdir函数：readdir函数用于读取目录流中的下一个边界，并返回一个指向struct dirent结构体的指针。
 closedir函数：closedir用于关闭目录流的函数，释放相关的资源。
 */
-typedef struct
-{
+typedef struct{
     char *filename;
-    char targetpath[MAX_PATH];
     struct stat info;
 } Fileinfo;
-
-void do_ls(char *dirname);
+void do_ls();
 int compare();
 int compare_t();
-void mode_letters(mode_t num, char *mode);
-void print_fileinfo(Fileinfo fileinfo);
-void print_filename(char *filename, mode_t filemode);
-void ls_R(char *dirname);
+void print_fileinfo();
+void print_filename();
 
 int has_a = 0;
 int has_l = 0;
@@ -82,7 +75,7 @@ int main(int argc, char **argv){
     for (int i = 1; i < argc; i++){
         if (argv[i][0] != '-'){
             flag = 1;
-            printf("%s\n:", argv[i]);
+            printf("%s:\n", argv[i]);
             do_ls(argv[i]);
         }
     }
@@ -92,95 +85,63 @@ int main(int argc, char **argv){
     return 0;
 }
 void do_ls(char *dirname){
-    // 先判断是否是链接文件
-    struct stat st;
-    if (lstat(dirname, &st) == -1){
-        perror("获取信息失败");
-        exit(1);
-    }
-    Fileinfo dirinfo;
-    dirinfo.filename = dirname;
-    dirinfo.info = st;
-    if (S_ISLNK(st.st_mode)){
-        ssize_t pathsize = readlink(dirname, dirinfo.targetpath, MAX_PATH - 1);
-        if (pathsize == -1){
-            perror("读取链接文件失败");
-            exit(1);
-        }
-        else
-            dirinfo.targetpath[pathsize] = '\0';
-
-        print_fileinfo(dirinfo);
+    Fileinfo fileinfo[4096];
+    int file_cnt = 0;
+    DIR *dir_ptr;
+    struct dirent *cur_dirent;
+    if ((dir_ptr = opendir(dirname)) == NULL){
+        perror("打开文件夹失败");
+        exit(EXIT_FAILURE);
     }
     else{
-        Fileinfo fileinfo[4096];
-        int file_cnt = 0;
-        DIR *dir_ptr;
-        struct dirent *cur_dirent;
-        if ((dir_ptr = opendir(dirname)) == NULL){
-            perror("打开文件夹失败");
-            exit(1);
+        while ((cur_dirent = readdir(dir_ptr)) != NULL){
+            if (!has_a && *(cur_dirent->d_name) == '.')
+                continue;
+            fileinfo[file_cnt++].filename = cur_dirent->d_name;
         }
-        else{
-            while ((cur_dirent = readdir(dir_ptr)) != NULL){
-                if (!has_a && *(cur_dirent->d_name) == '.')
-                    continue;
-                fileinfo[file_cnt++].filename = cur_dirent->d_name;
-            }
-        }
-        // 存储信息
-        for (int i = 0; i < file_cnt; i++){
-            char pathname[256];
-            strcpy(pathname, dirname);
-            strcat(pathname, "/");
-            strcat(pathname, fileinfo[i].filename);
-            if (lstat(pathname, &fileinfo[i].info) == -1){
-                perror("获取信息失败");
-                exit(1);
-            }
-            if (S_ISLNK(fileinfo[i].info.st_mode)){
-                ssize_t pathsize = readlink(pathname, fileinfo[i].targetpath, MAX_PATH - 1);
-                if (pathsize == -1){
-                    perror("读取链接文件失败");
-                    exit(1);
-                }
-                else
-                    fileinfo[i].targetpath[pathsize] = '\0';
-            }
-        }
-
-        if (has_t)
-            qsort(fileinfo, file_cnt, sizeof(Fileinfo), compare_t);
-        else
-            qsort(fileinfo, file_cnt, sizeof(Fileinfo), compare);
-            
-        if (has_r){
-            int left = 0, right = file_cnt - 1;
-            while (left < right){
-                Fileinfo temp = fileinfo[left];
-                fileinfo[left++] = fileinfo[right];
-                fileinfo[right--] = temp;
-            }
-        }
-        // 打印信息
-        for (int i = 0; i < file_cnt; i++)
-            print_fileinfo(fileinfo[i]);
-        if (has_R){
-            for (int i = 0; i < file_cnt; i++){
-                if (S_ISDIR(fileinfo[i].info.st_mode)){
-                    if (strcmp(fileinfo[i].filename, ".") || strcmp(fileinfo[i].filename, "..")){
-                        char pathname[256];
-                        strcpy(pathname, dirname);
-                        strcat(pathname, "/");
-                        strcat(pathname, fileinfo[i].filename);
-                        printf("\n%s:\n", pathname);
-                        do_ls(pathname);
-                    }
-                }
-            }
-        }
-        closedir(dir_ptr);
     }
+    // 存储信息
+    for (int i = 0; i < file_cnt; i++){
+        char pathname[256];
+        strcpy(pathname, dirname);
+        strcat(pathname, "/");
+        strcat(pathname, fileinfo[i].filename);
+        if (lstat(pathname, &fileinfo[i].info) == -1){
+            perror("获取信息失败");
+            exit(EXIT_FAILURE);
+        }
+    }
+    // 排序
+    if (has_t)
+        qsort(fileinfo, file_cnt, sizeof(Fileinfo), compare_t);
+    else
+        qsort(fileinfo, file_cnt, sizeof(Fileinfo), compare);
+    if (has_r){
+        int left = 0, right = file_cnt - 1;
+        while (left < right){
+            Fileinfo temp = fileinfo[left];
+            fileinfo[left++] = fileinfo[right];
+            fileinfo[right--] = temp;
+        }
+    }
+    // 打印信息
+    for (int i = 0; i < file_cnt; i++)
+        print_fileinfo(fileinfo[i]);
+    if (has_R){
+        for (int i = 0; i < file_cnt; i++){
+            if (S_ISDIR(fileinfo[i].info.st_mode)){
+                if (!strcmp(fileinfo[i].filename, ".") || !strcmp(fileinfo[i].filename, ".."))
+                    continue;
+                char pathname[256];
+                strcpy(pathname, dirname);
+                strcat(pathname, "/");
+                strcat(pathname, fileinfo[i].filename);
+                printf("\n%s: \n", pathname);
+                do_ls(pathname);
+            }
+        }
+    }
+    closedir(dir_ptr);
 }
 //比较函数
 int compare(const void *a, const void *b){
@@ -196,7 +157,6 @@ int compare_t(const void *a, const void *b){
 // 将权限转换为字符串
 void mode_letters(mode_t num, char *mode) {
     strcpy(mode, "----------");
-    // 判断文件类型
     switch (num & __S_IFMT){
     case __S_IFREG: /* Regular file.  */
         mode[0] = '-';
@@ -245,31 +205,29 @@ void mode_letters(mode_t num, char *mode) {
 
 void print_fileinfo(const Fileinfo fileinfo){
     if (has_i)
-        printf("%-4lu ", fileinfo.info.st_ino);
+        printf("%-8lu ", fileinfo.info.st_ino);
     if (has_s)
-        printf("%-4ld ", (long)fileinfo.info.st_size);
+        printf("%-8ld ", (long)fileinfo.info.st_size/1024);
     if (has_l){
         char mode[11];
         mode_letters(fileinfo.info.st_mode, mode);
-        printf("%s  ", mode);
+        printf("%s ", mode);
 
-        printf("%-2d  ", (int)fileinfo.info.st_nlink); // 打印链接数
+        printf("%-2d ", (int)fileinfo.info.st_nlink); // 打印链接数
 
         struct passwd *user;
         user = getpwuid(fileinfo.info.st_uid);
-        printf("%s  ", user->pw_name); // 打印用户名
+        printf("%s ", user->pw_name); // 打印用户名
 
         struct group *gp;
         gp = getgrgid(fileinfo.info.st_gid);
-        printf("%s  ", gp->gr_name); // 打印组名
+        printf("%s ", gp->gr_name); // 打印组名
 
-        printf("%-10ld  ", fileinfo.info.st_size);             // 打印文件大小
-        printf("%.12s  ", ctime(&fileinfo.info.st_mtime) + 4); // 打印时间
+        printf("%-10ld ", fileinfo.info.st_size);             // 打印文件大小
+        printf("%.12s ", ctime(&fileinfo.info.st_mtime) + 4); // 打印时间
     }
 
     print_filename(fileinfo.filename, fileinfo.info.st_mode);
-    if (S_ISLNK(fileinfo.info.st_mode) && has_l)
-        printf(" -> %s", fileinfo.targetpath);
     printf("\n");
 }
 // 染色文件名
